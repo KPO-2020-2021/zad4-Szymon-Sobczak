bb2874bed7cee8cdde6843334e826bffd427ae6e
SzS <259275@student.pwr.edu.pl>
2021-05-14 23:49:58 +0200

    Pierwszy commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ader/Pulpit/Zad 4 Obroty 3D GIT/zad4-Szymon-Sobczak
+# Build directory: /home/ader/Pulpit/Zad 4 Obroty 3D GIT/zad4-Szymon-Sobczak/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
